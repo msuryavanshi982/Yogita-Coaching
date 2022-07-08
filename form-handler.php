c7b432cf6fd2afafc4e96eed6650d9302a06915d
Yogita Suryavanshi Rajput <rajputmeenakshi982@gmail.com>
2022-07-08 19:18:12 +0530

    coaching web

---
+++ form-handler.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = "yogitacoaching@gmail.com"
+
+
+
+
+
+
+?>
